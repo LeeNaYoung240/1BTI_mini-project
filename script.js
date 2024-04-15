2548535ace9f5812e268231cf2bffec7e958cc89
조현아 <minjokwindows5@gmail.com>
2024-04-15 20:44:53 +0900

    기본 html 작성

---
+++ script.js
@@ -0,0 +1,36 @@
+
+$('#answer01').click(async function(){
+    location.href="question2.html";
+    document.cookie='q1=01';
+})
+$('#answer02').click(async function(){
+    location.href="question2.html";
+    document.cookie='q1=02';
+})
+
+$('#answer03').click(async function(){
+    location.href="question3.html";
+    document.cookie='q2=01';
+})
+$('#answer04').click(async function(){
+    location.href="question3.html";
+    document.cookie='q2=02';
+})
+
+$('#answer05').click(async function(){
+    location.href="question4.html";
+    document.cookie='q3=01';
+})
+$('#answer06').click(async function(){
+    location.href="question4.html";
+    document.cookie='q3=02';
+})
+
+$('#answer07').click(async function(){
+    location.href="result.html";
+    document.cookie='q4=01';
+})
+$('#answer08').click(async function(){
+    location.href="result.html";
+    document.cookie='q4=02';
+})
